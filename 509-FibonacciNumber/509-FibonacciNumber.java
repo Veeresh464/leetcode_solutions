74e2904b003b9ef45f0a593539e97d191f8a43c1
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-13 21:46:39 +0530

    File updated at 2/13/2025, 9:46:39 PM

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,14 +1,18 @@
 class Solution {
-    //tabular
-    public int fib(int n) {
+
+
+    int getAns(int n, int[]dp){
         if(n==0) return 0;
         if(n==1) return 1;
-        int dp[] = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i=2; i<dp.length; i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+
+        if(dp[n] != -1) return dp[n];
+        int ans = getAns(n-1, dp) + getAns(n-2, dp);
+        return dp[n]=ans;
+    }
+
+    public int fib(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return getAns(n, dp);
     }
 }
