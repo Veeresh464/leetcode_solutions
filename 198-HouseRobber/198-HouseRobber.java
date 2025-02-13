54ed0a46a45b29c06962166a03d97eb03f104085
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-13 22:29:21 +0530

    File updated at 2/13/2025, 10:29:22 PM

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,18 +1,33 @@
 class Solution {
 
-    int getAns(int []nums, int idx, int[] dp){
-        if(idx >= nums.length) return 0;
+    //recursion
+    // int getAns(int []nums, int idx, int[] dp){
+    //     if(idx >= nums.length) return 0;
 
-        if(dp[idx] != -1) return dp[idx];
-        int skip = getAns(nums, idx+1, dp);
-        int take = nums[idx] + getAns(nums, idx+2, dp);
+    //     if(dp[idx] != -1) return dp[idx];
+    //     int skip = getAns(nums, idx+1, dp);
+    //     int take = nums[idx] + getAns(nums, idx+2, dp);
 
-        return dp[idx] = Math.max(skip, take);
-    }
+    //     return dp[idx] = Math.max(skip, take);
+    // }
+    // public int rob(int[] nums) {
+    //     int dp[] = new int[nums.length];
+    //     Arrays.fill(dp,-1);
+    //     return getAns(nums, 0, dp);
+    // }
 
+    //tabulation
     public int rob(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
         int dp[] = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return getAns(nums, 0, dp);
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[1],nums[0]);
+
+        for(int i=2; i<nums.length; i++){
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+        }
+        return dp[dp.length -1];
     }
+
 }
