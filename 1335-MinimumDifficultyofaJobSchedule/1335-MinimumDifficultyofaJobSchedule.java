c6176663d1cede0772f374592b4a8857b27e1826
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-16 10:25:15 +0530

    File updated at 2/16/2025, 10:25:15 AM

--- 1335-MinimumDifficultyofaJobSchedule/1335-MinimumDifficultyofaJobSchedule.java
+++ 1335-MinimumDifficultyofaJobSchedule/1335-MinimumDifficultyofaJobSchedule.java
@@ -1,39 +1,31 @@
 class Solution {
-
-
-    int getAns(int[] arr, int d, int idx, int [][] dp){
-
-        if(d==1){
-            int m = arr[idx];
-            for(int j=idx; j<arr.length; j++){
-                if(arr[j]>m) m=arr[j];
-            }
-            return m;
-        }
-        if(dp[idx][d] != -1) return dp[idx][d];
-        int maxD=arr[idx];
-        int f = Integer.MAX_VALUE;
-        for(int i=idx; i<=arr.length-d; i++){
-           maxD = Math.max(arr[i], maxD);
-            int res = maxD+getAns(arr, d-1, i+1, dp);
-            f = Math.min(f,res);
-        }
-        return dp[idx][d]=f;
-    }
-
-    public int minDifficulty(int[] arr, int d) {
-        if(d>arr.length) return -1;
+    int getAns(int []arr, int d, int idx, int dp[][]){
         if(d == 1){
-            int max = arr[0];
-            for(int i=1; i<arr.length; i++){
-                if(arr[i] > max) max=arr[i];
+            int max = arr[idx];
+            for(int i=idx; i<arr.length; i++){
+                if(arr[i]>max) max = arr[i];
             }
             return max;
         }
-        int dp[][] = new int[arr.length][d+1];
+        
+        if(dp[idx][d] != -1) return dp[idx][d];
+
+        int maxD = arr[idx];
+        int finalD = Integer.MAX_VALUE;
+        for(int i=idx; i<=arr.length-d; i++){
+            maxD = Math.max(maxD, arr[i]);
+            int res = getAns(arr,d-1,i+1,dp);
+            finalD = Math.min(finalD, res+maxD);
+        }
+        return dp[idx][d] = finalD;
+    }
+
+    public int minDifficulty(int[] jobDifficulty, int d) {
+        if(d>jobDifficulty.length) return -1;
+        int dp[][] = new int[jobDifficulty.length][d+1];
         for(int row[]:dp){
             Arrays.fill(row,-1);
         }
-        return getAns(arr,d,0,dp);
+        return getAns(jobDifficulty,d,0,dp);
     }
 }
