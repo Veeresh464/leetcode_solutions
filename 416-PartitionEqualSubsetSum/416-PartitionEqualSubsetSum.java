ab52f5f6637ea7ae618fd33dc9ef9a3fc6eae459
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-18 21:58:40 +0530

    File updated at 2/18/2025, 9:58:41 PM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,80 +1,36 @@
 class Solution {
-	// int getAns(int []arr, int c, int i, int dp[][]) {
-	// 	if(c == 0) return 1;
-	// 	if(i >= arr.length || c < 0) return 0;
-		
-	// 	if(dp[i][c] != -1) return dp[i][c];
-	// 	int skip = getAns(arr,c,i+1, dp);
-	// 	int take = getAns(arr,c-arr[i], i+1, dp);
-		
-	// 	if(skip == 1|| take ==1) return dp[i][c]=1;
-	// 	return dp[i][c]=0;
-	// }
 
-    // public boolean canPartition(int[] nums) {
-    //     int sum = 0;
-    //     for(int e:nums){
-    //         sum+=e;
-    //     }
-    //     if(sum%2 == 1) return false;
-    //     int tgt = sum/2;
-    //     int dp[][] = new int[nums.length][tgt+1];
-    //     for(int row[] : dp){
-    //         Arrays.fill(row,-1);
-    //     }
-    //     if(getAns(nums, tgt, 0, dp) == 1) return true;
-    //     else return false; 
-    // }
+    boolean getAns(int nums[], int tgt, int idx, int dp[][]){
+        // if(idx == nums.length){
+        //     if(tgt == 0) return true;
+        //     else return false;
+        // }
+        if(tgt == 0) return true;
+        if(idx >= nums.length || tgt < 0) return false;
 
-
-    // int getAns(int []arr, int c, int i, int dp[][]) {
-	// 	if(c == 0) return 1;
-	// 	if(i < 0 || c < 0) return 0;
-		
-	// 	if(dp[i][c] != -1) return dp[i][c];
-	// 	int skip = getAns(arr,c,i-1, dp);
-	// 	int take = getAns(arr,c-arr[i], i-1, dp);
-		
-	// 	if(skip == 1|| take ==1) return dp[i][c]=1;
-	// 	return dp[i][c]=0;
-	// }
-
-    // public boolean canPartition(int[] nums) {
-    //     int sum = 0;
-    //     for(int e:nums){
-    //         sum+=e;
-    //     }
-    //     if(sum%2 == 1) return false;
-    //     int tgt = sum/2;
-    //     int dp[][] = new int[nums.length][tgt+1];
-    //     for(int row[] : dp){
-    //         Arrays.fill(row,-1);
-    //     }
-    //     if(getAns(nums, tgt, nums.length-1, dp) == 1) return true;
-    //     else return false; 
-    // }
-
+        if(dp[idx][tgt] != -1){
+            if(dp[idx][tgt] == 1) return true;
+            else return false;
+        } 
+        boolean skip = getAns(nums,tgt,idx+1,dp);
+        boolean take = getAns(nums,tgt-nums[idx], idx+1,dp);
+        if(skip || take) dp[idx][tgt] = 1;
+        else dp[idx][tgt] = 0;
+        return skip||take;
+    }
 
     public boolean canPartition(int[] nums) {
         int sum = 0;
-        for(int e:nums){
-            sum+=e;
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i];
         }
         if(sum%2 == 1) return false;
+
         int tgt = sum/2;
         int dp[][] = new int[nums.length][tgt+1];
-        
-        for(int i=0; i<dp.length; i++){
-            for(int j=0; j<dp[0].length; j++){
-                if(j == 0) dp[i][j] = 1;
-                else{
-                    int skip = (i-1 >= 0)? dp[i-1][j]:0;
-                    int take = (i-1 >= 0 && j-nums[i]>=0)? dp[i-1][j-nums[i]]:0;
-                    dp[i][j] = Math.max(take,skip);
-                }
-            }
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
         }
-        if(dp[dp.length-1][dp[0].length-1] == 1) return true;
-        else return false;
+        return getAns(nums,tgt,0,dp);
     }
 }
