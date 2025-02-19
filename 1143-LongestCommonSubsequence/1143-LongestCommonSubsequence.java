67e6bf1a2831f00ec3004740963358f400e38440
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-19 15:16:36 +0530

    File updated at 2/19/2025, 3:16:35 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,40 +1,23 @@
 class Solution {
-    // recursive
-    // int getAns(StringBuilder text1, StringBuilder text2, int t1, int t2, int [][] dp){
-    //     if(t1<0 || t2<0) return 0;
 
-    //     int res=0;
-    //     if(dp[t1][t2] != -1) return dp[t1][t2];
-    //     if(text1.charAt(t1) == text2.charAt(t2)){
-    //         res+= 1+ getAns(text1, text2, t1-1, t2-1, dp);
-    //     }
-    //     else{
-    //         res = Math.max(getAns(text1, text2, t1-1, t2, dp), getAns(text1, text2, t1, t2-1, dp));
-    //     }
-    //     return dp[t1][t2]=res;
-    // }
-    // public int longestCommonSubsequence(String text1, String text2) {
-    //     StringBuilder a = new StringBuilder(text1);
-    //     StringBuilder b = new StringBuilder(text2);
-    //     int [][] dp = new int[a.length()][b.length()];
-    //     for(int row[]:dp){
-    //         Arrays.fill(row,-1);
-    //     }
-    //     return getAns(a, b, a.length()-1, b.length()-1, dp);
-    // }
+    int getAns(String t1, String t2, int i1, int i2, int dp[][]){
+        if(i1 < 0 || i2 < 0) return 0;
 
-    // tabulation
-    public int longestCommonSubsequence(String text1, String text2) {
-        StringBuilder a = new StringBuilder(text1);
-        StringBuilder b = new StringBuilder(text2);
-        int [][] dp = new int[a.length()][b.length()];
-        for(int i=0; i<a.length(); i++){
-            for(int j=0; j<b.length(); j++){
-                if(a.charAt(i) == b.charAt(j)) dp[i][j] = 1+ ((i-1 >= 0 && j-1 >= 0)? dp[i-1][j-1]:0);
-                else dp[i][j] = Math.max( (i-1 >= 0)? dp[i-1][j]:0 , (j-1 >= 0)? dp[i][j-1]:0);
-            }
+        int common = 0;
+        int take1 = 0;
+        if(dp[i1][i2] != -1) return dp[i1][i2];
+        if(t1.charAt(i1) == t2.charAt(i2)){
+            common = 1+getAns(t1,t2,i1-1,i2-1, dp);
         }
-        return dp[a.length()-1][b.length()-1];
+        else take1 = Math.max(getAns(t1, t2, i1-1, i2, dp),getAns(t1,t2,i1,i2-1, dp));
+        return dp[i1][i2] = take1+common;
     }
 
+    public int longestCommonSubsequence(String text1, String text2) {
+        int dp[][] = new int[text1.length()][text2.length()];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
+        }
+        return getAns(text1, text2, text1.length()-1, text2.length()-1, dp);
+    }
 }
