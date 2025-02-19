4d9efba73397acc574f86a751cf084d0cc81c811
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-19 22:24:34 +0530

    File updated at 2/19/2025, 10:24:33 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,42 +1,28 @@
 class Solution {
 
 
-    // public int getAns(int[] coins, int amount, int idx, int sum){
-
-    //     if(idx >= coins.length || amount <= 0) return sum;
-        
-    //     int skip = getAns(coins, amount, idx+1, sum+1);
-    //     int take = getAns(coins, amount-coins[idx], idx, sum+1);
-
-    //     return Math.min(skip, take);
-    // }
-    // public int coinChange(int[] coins, int amount) {
-    //     return getAns(coins, amount, 0, 0);
-    // }
-
-    public int getAns(int[] coins, int amount, int idx, int dp[][]){
-
-        if(idx == coins.length){
-            if(amount == 0) return 0;
-            else return Integer.MAX_VALUE-1;
+    int getAns(int arr[], int amt, int idx, int[][]dp){
+        if(arr.length == idx){
+            if(amt == 0) return 0;
+            else return Integer.MAX_VALUE;
         }
+        if(dp[idx][amt] != -1) return dp[idx][amt];
+        int skip = getAns(arr, amt, idx+1, dp);
+        if(amt - arr[idx] < 0) return dp[idx][amt]=skip;
+        int take = getAns(arr, amt-arr[idx], idx, dp);
+        if(take != Integer.MAX_VALUE){
+            take++;
+        }
+        return dp[idx][amt]=Math.min(skip, take);
+    }   
 
-        if(dp[idx][amount] != -1) return dp[idx][amount];
-        int skip = getAns(coins, amount, idx+1, dp);
-        if(amount-coins[idx] < 0) return skip;
-        int take = 1 + getAns(coins, amount-coins[idx], idx, dp);
-
-        return dp[idx][amount] = Math.min(take,skip);
-        
-    }
-
-
-    public int coinChange(int[] coins, int amount) {
-        int dp[][] = new int[coins.length][amount+1];
-        for(int row[]:dp){
+    public int coinChange(int[] arr, int amt) {
+        int dp[][] = new int[arr.length][amt+1];
+        for(int row[]: dp){
             Arrays.fill(row,-1);
         }
-        int ans=getAns(coins, amount, 0, dp);
-        return ans>214740000 ? -1: ans;
+        int ans = getAns(arr,amt, 0, dp);
+        System.out.println(ans);
+        return (ans <= 2147483000)? ans:-1;
     }
 }
