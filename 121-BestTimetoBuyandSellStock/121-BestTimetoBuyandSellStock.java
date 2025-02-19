832a603e4f487819a93624f75b896dfaebdd1b7c
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-19 16:55:43 +0530

    File updated at 2/19/2025, 4:55:43 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,27 +1,14 @@
 class Solution {
-
-
-    int getAns(int []arr, int idx, int min){
+    int getAns(int arr[], int idx, int min){
         if(idx >= arr.length) return 0;
 
-        min = Math.min(arr[idx], min);
-        int profit = Math.max(arr[idx]-min, getAns(arr,idx+1, min));
-        
+        min = Math.min(min,arr[idx]);
+        int profit = Math.max(arr[idx]-min, getAns(arr, idx+1, min));
+
         return profit;
     }
 
     public int maxProfit(int[] arr) {
-        return getAns(arr,1,arr[0]);
+        return getAns(arr,0,Integer.MAX_VALUE);
     }
-
-
-    // public int maxProfit(int[] arr) {
-    //     int min = arr[0];
-    //     int profit = 0;
-    //     for(int i=1; i<arr.length; i++){
-    //         min = Math.min(min, arr[i]);
-    //         profit = Math.max(profit, arr[i] - min);
-    //     }
-    //     return profit;
-    // }
 }
