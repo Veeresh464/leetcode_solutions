8cfc18460fe881ef8d75678597a40cbee8a017ad
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-20 23:00:42 +0530

    File updated at 2/20/2025, 11:00:42 PM

--- 841-KeysandRooms/841-KeysandRooms.java
+++ 841-KeysandRooms/841-KeysandRooms.java
@@ -3,7 +3,6 @@ class Solution {
         ArrayDeque <Integer> q = new ArrayDeque<>();
         q.offer(idx);
         visited[idx]++;
-
         while(!q.isEmpty()){
             int rem = q.poll();
             for(int i=0; i<arr.get(rem).size(); i++){
@@ -15,12 +14,27 @@ class Solution {
         }
     }
 
+    void DFS(List<List<Integer>> arr, int visited[], int idx){
+        Stack <Integer> st = new Stack<>();
+        st.push(idx);
+        visited[idx]++;
+        while(!st.isEmpty()){
+            int rem = st.pop();
+            for(int i=0; i<arr.get(rem).size(); i++){
+                if(visited[arr.get(rem).get(i)] == 0){
+                    st.push(arr.get(rem).get(i));
+                    visited[arr.get(rem).get(i)]++;
+                }
+            }
+        }
+    }
 
     public boolean canVisitAllRooms(List<List<Integer>> arr) {
         int n = arr.size();
         int visited[] = new int[n];
 
-        BFS(arr, visited, 0);
+        // BFS(arr, visited, 0);
+        DFS(arr, visited, 0);
         boolean allTraversed = true;
         for(int i=0; i<n; i++){
             if(visited[i] == 0){
