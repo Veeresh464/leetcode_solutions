59cb3c13db427154b06c9ede32893c8944b26fe6
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-20 18:35:44 +0530

    File updated at 2/20/2025, 6:35:45 PM

--- 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.java
+++ 516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.java
@@ -1,41 +1,35 @@
 class Solution {
 
-    // int getAns(StringBuilder text1, StringBuilder text2, int t1, int t2, int [][] dp){
-    //     if(t1<0 || t2<0) return 0;
 
-    //     int res=0;
-    //     if(dp[t1][t2] != -1) return dp[t1][t2];
-    //     if(text1.charAt(t1) == text2.charAt(t2)){
-    //         res+= 1+ getAns(text1, text2, t1-1, t2-1, dp);
-    //     }
-    //     else{
-    //         res = Math.max(getAns(text1, text2, t1-1, t2, dp), getAns(text1, text2, t1, t2-1, dp));
-    //     }
-    //     return dp[t1][t2]=res;
-    // }
+    int getLCS(String s1, String s2, int l1, int l2, int[][]dp){
+        if(l1 < 0 || l2 < 0){
+            return 0;
+        }
+
+
+        if(dp[l1][l2] != -1) return dp[l1][l2];
+        int ans = 0;
+        int ans2 = 0;
+        if(s1.charAt(l1) == s2.charAt(l2)){
+            ans = 1 + getLCS(s1,s2,l1-1,l2-1,dp);
+        }
+        else{
+            int take1 = getLCS(s1,s2,l1-1,l2,dp);
+            int take2 = getLCS(s1,s2,l1,l2-1,dp);
+            ans2 = Math.max(take1, take2);
+        }
+
+        return dp[l1][l2]=ans+ans2;
+    }
+
 
     public int longestPalindromeSubseq(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
-
-        // StringBuilder s1 = new StringBuilder(s);
-        // StringBuilder s2 = new StringBuilder(rev);
-        // int [][] dp = new int[s1.length()][s2.length()];
-        // for(int row[]:dp){
-        //     Arrays.fill(row,-1);
-        // }
-        // return getAns(s1,s2,s1.length()-1,s2.length()-1,dp);
-
-        StringBuilder a = new StringBuilder(s);
-        StringBuilder b = new StringBuilder(rev);
-
-        int [][] dp = new int[a.length()][b.length()];
-        for(int i=0; i<a.length(); i++){
-            for(int j=0; j<b.length(); j++){
-                if(a.charAt(i) == b.charAt(j)) dp[i][j] = 1+ ((i-1 >= 0 && j-1 >= 0)? dp[i-1][j-1]:0);
-                else dp[i][j] = Math.max( (i-1 >= 0)? dp[i-1][j]:0 , (j-1 >= 0)? dp[i][j-1]:0);
-            }
+        int n = s.length();
+        int dp[][] = new int[n][n];
+        for(int row[]:dp){
+            Arrays.fill(row,-1);
         }
-        return dp[a.length()-1][b.length()-1];
-        
+        String str = new StringBuilder(s).reverse().toString();
+        return getLCS(s,str, s.length()-1, str.length()-1, dp);
     }
 }
