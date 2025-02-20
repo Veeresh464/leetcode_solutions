d5e7f886fa6a7a0511ac23858bccbe78d84cb80f
Veeresh464 <120156473+Veeresh464@users.noreply.github.com>
2025-02-20 22:52:57 +0530

    Create file

---
+++ 841-KeysandRooms/841-KeysandRooms.java
@@ -0,0 +1,33 @@
+class Solution {
+    void BFS(List<List<Integer>> arr, int []visited, int idx){
+        ArrayDeque <Integer> q = new ArrayDeque<>();
+        q.offer(idx);
+        visited[idx]++;
+
+        while(!q.isEmpty()){
+            int rem = q.poll();
+            for(int i=0; i<arr.get(rem).size(); i++){
+                if(visited[arr.get(rem).get(i)] == 0){
+                    q.offer(arr.get(rem).get(i));
+                    visited[arr.get(rem).get(i)]++;
+                }
+            }
+        }
+    }
+
+
+    public boolean canVisitAllRooms(List<List<Integer>> arr) {
+        int n = arr.size();
+        int visited[] = new int[n];
+
+        BFS(arr, visited, 0);
+        boolean allTraversed = true;
+        for(int i=0; i<n; i++){
+            if(visited[i] == 0){
+                allTraversed = false;
+                break;
+            }
+        }
+        return allTraversed;
+    }
+}
